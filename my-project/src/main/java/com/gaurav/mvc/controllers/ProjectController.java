af7ff6d52d3a858f1238f529f30467deb8a8caed
gaurav <gaurav.matta@gmail.com>
2019-01-10 22:36:13 +0530

    added Request Mapping

---
+++ my-project/src/main/java/com/gaurav/mvc/controllers/ProjectController.java
@@ -0,0 +1,39 @@
+package com.gaurav.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+	@RequestMapping(value="/add",method=RequestMethod.GET)
+	public String addProject()
+	{
+		System.out.println("Invoking addProject");
+		return "project_add";
+	}
+	
+	@RequestMapping(value="/add",method=RequestMethod.POST)
+	public String saveProject()
+	{
+		System.out.println("Invoking saveProject");
+		return "project_add";
+	}
+	
+	@RequestMapping(value="/add",method=RequestMethod.POST,params={"type=multi"})
+	public String saveMulti()
+	{
+		System.out.println("Invoking saveMulti");
+		return "project_add";
+	}
+	
+	@RequestMapping(value="/add",method=RequestMethod.POST,params={"type=multi","special"})
+	public String saveSpecial()
+	{
+		System.out.println("Invoking saveSpecial");
+		return "project_add";
+	}
+	
+}
