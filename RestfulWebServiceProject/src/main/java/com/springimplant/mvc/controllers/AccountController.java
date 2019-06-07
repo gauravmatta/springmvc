9f4d20b723b76de174c954f892353b8543b89422
Gaurav Matta <gaurav.matta@gmail.com>
2019-06-07 19:14:20 +0530

    Added RestController in place of ResponseBody

--- RestfulWebServiceProject/src/main/java/com/springimplant/mvc/controllers/AccountController.java
+++ RestfulWebServiceProject/src/main/java/com/springimplant/mvc/controllers/AccountController.java
@@ -3,16 +3,14 @@ package com.springimplant.mvc.controllers;
 import java.math.BigDecimal;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springimplant.mvc.model.Account;
 
-@Controller
+@RestController
 @RequestMapping("/v1/forecasting")
-@ResponseBody
 public class AccountController {
 
 	@RequestMapping(value="accounts",method=RequestMethod.GET,produces={MediaType.APPLICATION_JSON_VALUE})
