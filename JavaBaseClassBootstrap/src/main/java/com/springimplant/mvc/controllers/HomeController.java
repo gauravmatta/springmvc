232fa7123aed421667e1dc1a4cfd29828f1259ad
Gaurav Matta <gaurav.matta@gmail.com>
2019-06-08 21:33:28 +0530

    Removed page tag from jsp files

--- JavaBaseClassBootstrap/src/main/java/com/springimplant/mvc/controllers/HomeController.java
+++ JavaBaseClassBootstrap/src/main/java/com/springimplant/mvc/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.springimplant.mvc.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
