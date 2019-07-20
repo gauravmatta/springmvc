cd31823a48c51114a1d7598c2873b271b17ea073
Gaurav Matta <gaurav.matta@gmail.com>
2019-07-20 19:46:16 +0530

    Added DI

--- spring-welcome/src/com/springimplant/entities/Student.java
+++ spring-welcome/src/com/springimplant/entities/Student.java
@@ -2,11 +2,28 @@ package com.springimplant.entities;
 
 public class Student {
 
-	String studentName;
+	private int id;
+	private String studentName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
 
 	@Override
 	public String toString() {
-		return "Student [studentName=" + studentName + "]";
+		return "Student [id=" + id + ", studentName=" + studentName + "]";
 	}
 	
 }
--- spring-welcome/src/com/springimplant/main/Exam.java
+++ spring-welcome/src/com/springimplant/main/Exam.java
@@ -1,5 +1,21 @@
 package com.springimplant.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springimplant.entities.Student;
+
 public class Exam {
 
+	public static void main(String args[])
+	{
+//		Student student=new Student();
+//		student.setStudentName("Gaurav Matta");
+//		System.out.println(student);
+		ApplicationContext context=new ClassPathXmlApplicationContext("/resources/beans.xml");
+		Student gaurav=context.getBean("student",Student.class);
+		System.out.println(gaurav);
+		Student mohit=context.getBean("raghavi",Student.class);
+		System.out.println(mohit);
+	}
 }
