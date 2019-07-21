8a606441fbd9e3fb5225524057bc02d6b52d2bf2
Gaurav Matta <gaurav.matta@gmail.com>
2019-07-21 17:46:31 +0530

    Added Spring AOP programming

---
+++ spring-aop-project/src/com/springimplant/main/AppMain.java
@@ -0,0 +1,16 @@
+package com.springimplant.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springimplant.service.ShapeService;
+
+public class AppMain 
+{
+	public static void main(String args[])
+	{
+		ApplicationContext context=new ClassPathXmlApplicationContext("/resources/spring.xml");
+		ShapeService shapeService=context.getBean("shapeService",ShapeService.class);
+		System.out.println(shapeService.getCircle().getName());
+	}
+}
---
+++ spring-aop-project/src/com/springimplant/service/ShapeService.java
@@ -0,0 +1,21 @@
+package com.springimplant.service;
+
+import com.springimplant.entities.Circle;
+import com.springimplant.entities.Triangle;
+
+public class ShapeService {
+private Circle circle;
+private Triangle triangle;
+public Circle getCircle() {
+	return circle;
+}
+public void setCircle(Circle circle) {
+	this.circle = circle;
+}
+public Triangle getTriangle() {
+	return triangle;
+}
+public void setTriangle(Triangle triangle) {
+	this.triangle = triangle;
+}
+}
