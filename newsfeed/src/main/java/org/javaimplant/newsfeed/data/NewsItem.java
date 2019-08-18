98f02011127c845c605d63fa4c1d4d7a2df2d2c6
Gaurav Matta <Gaurav Matta@DESKTOP-U9M9DGO>
2019-08-18 23:56:13 +0530

    Added DataAccessObject

---
+++ newsfeed/src/main/java/org/javaimplant/newsfeed/data/NewsItem.java
@@ -0,0 +1,27 @@
+package org.javaimplant.newsfeed.data;
+
+public class NewsItem 
+{	
+	private Long id;
+	private String title;
+	private String url;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
