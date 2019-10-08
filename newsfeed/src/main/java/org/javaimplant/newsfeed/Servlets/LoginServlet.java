3ed84c8635f2b4180687337da3a13f8b3f446b89
gaurav <gaurav@home>
2019-10-08 23:18:31 +0530

    Added Login Functionality

---
+++ newsfeed/src/main/java/org/javaimplant/newsfeed/Servlets/LoginServlet.java
@@ -0,0 +1,51 @@
+package org.javaimplant.newsfeed.Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.javaimplant.newsfeed.data.User;
+import org.javaimplant.newsfeed.data.UserDAO;
+
+public class LoginServlet extends ParentServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		logger.debug("doGet()");
+		jsp.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		logger.debug("doPost()");
+		String username = req.getParameter("username");
+		User user = new UserDAO().findByUsername(username);
+		if (user == null)
+		{
+			logger.debug("authentication failed: bad username");
+			req.setAttribute("message", "Authentication failed.");
+			jsp.forward(req, resp);
+			return;
+		}
+		String password = req.getParameter("password");
+		if (password == null || !user.getPassword().equals(password))
+		{
+			logger.debug("authentication failed: bad password");
+			req.setAttribute("message", "Authentication failed.");
+			jsp.forward(req, resp);
+			return;
+		}
+		HttpSession session = req.getSession();
+		Long userId = user.getId();
+		session.setAttribute("userId", userId);
+		logger.debug("authenticated");
+		String url = "home";
+		resp.sendRedirect(url);
+	}
+
+}
---
+++ newsfeed/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Publisher</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+	<h1>Login</h1>
+	<%
+		String message = (String) request.getAttribute("message");
+		if (message != null) {
+			out.println("<p>" + message + "</p>");
+		}
+	%>
+	<form method="post" action="">
+		<div>
+			Username: <input type="text" name="username" size="36" />
+		</div>
+		<div>
+			Password: <input type="password" name="password" size="36" />
+		</div>
+		<div>
+			<input type="submit" value="Login" />
+		</div>
+	</form>
+</html>
