95af2de193db220dd8d5ada9c1ea2bf3d6eb466b
gaurav <gaurav@home>
2019-10-16 00:15:20 +0530

    Added Security Filter and updated web.xml

---
+++ newsfeed/src/main/java/org/javaimplant/newsfeed/Filters/SecurityFilter.java
@@ -0,0 +1,62 @@
+package org.javaimplant.newsfeed.Filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+public class SecurityFilter implements Filter {
+	
+	private Logger logger = Logger.getLogger(this.getClass());
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		logger.debug("doFilter()");
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		String servletPath = req.getServletPath();
+		
+		if (servletPath.equals("/login"))
+		{
+			chain.doFilter(req, resp);
+			return;
+		}
+		
+		if (servletPath.equals("/news.rss")) 
+		{
+			chain.doFilter(req, resp);
+			return;
+		}
+		HttpSession session = req.getSession();
+		Long userId = (Long) session.getAttribute("userId");
+		if (userId != null)
+		{
+			chain.doFilter(req, resp);
+			return;
+		}
+		resp.sendRedirect("login");
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}	
+}
