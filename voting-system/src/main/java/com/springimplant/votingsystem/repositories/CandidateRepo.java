d3303d1931f7f3ebe9c2e77fba19c746df45829f
gaurav <gaurav.matta@gmail.com>
2020-02-08 13:53:31 +0530

    Added Candidate Repository

---
+++ voting-system/src/main/java/com/springimplant/votingsystem/repositories/CandidateRepo.java
@@ -0,0 +1,11 @@
+package com.springimplant.votingsystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springimplant.votingsystem.entity.Candidate;
+
+@Repository
+public interface CandidateRepo extends JpaRepository<Candidate,Integer> {
+
+}
