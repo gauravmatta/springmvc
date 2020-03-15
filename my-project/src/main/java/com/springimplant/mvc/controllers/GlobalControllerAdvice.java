0a82b081629e67d2a5b5e8dc465f6d599ff8bbfb
gaurav <gaurav.matta@gmail.com>
2020-03-16 00:16:45 +0530

    Fixed issues in running my project on java 1.8

--- my-project/src/main/java/com/springimplant/mvc/controllers/GlobalControllerAdvice.java
+++ my-project/src/main/java/com/springimplant/mvc/controllers/GlobalControllerAdvice.java
@@ -2,19 +2,14 @@ package com.springimplant.mvc.controllers;
 
 import java.util.Date;
 
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import com.springimplant.mvc.data.validators.ProjectValidator;
 
 @ControllerAdvice(annotations = Controller.class)
 public class GlobalControllerAdvice {
