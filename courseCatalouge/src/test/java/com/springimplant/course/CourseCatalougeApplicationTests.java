748a1daa90217b3c69aeaabd97cd687d6f41a8cf
Gaurav Matta <gaurav.matta@gmail.com>
2020-04-22 22:41:46 +0530

    initial commit

---
+++ courseCatalouge/src/test/java/com/springimplant/course/CourseCatalougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springimplant.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCatalougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
