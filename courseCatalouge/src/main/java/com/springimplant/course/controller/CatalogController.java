09cd350ac21b3f100350899fb8b8b11fa937c1c5
Gaurav Matta <gaurav.matta@gmail.com>
2020-05-01 00:18:52 +0530

    Added Eureka Server App

--- courseCatalouge/src/main/java/com/springimplant/course/controller/CatalogController.java
+++ courseCatalouge/src/main/java/com/springimplant/course/controller/CatalogController.java
@@ -4,7 +4,6 @@ import java.math.BigInteger;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
