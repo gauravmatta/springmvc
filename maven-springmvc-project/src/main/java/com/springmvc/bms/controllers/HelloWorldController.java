2c07c4467e8a17c1a20100c2c786acaa4aa19396
Gaurav Matta <gaurav.matta@gmail.com>
2020-05-01 16:56:10 +0530

    updated projects

--- maven-springmvc-project/src/main/java/com/springmvc/bms/controllers/HelloWorldController.java
+++ maven-springmvc-project/src/main/java/com/springmvc/bms/controllers/HelloWorldController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HelloWorldController {
 
-	   @RequestMapping(method = RequestMethod.GET)
+	   @RequestMapping(value="hello",method = RequestMethod.GET)
 	    public String sayHello(ModelMap model) {
 	        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
 	        return "welcome";
