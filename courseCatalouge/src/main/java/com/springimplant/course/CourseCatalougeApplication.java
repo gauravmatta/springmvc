36e56613fb04521c487b2b15ac37f927412afa2d
Gaurav Matta <gaurav.matta@gmail.com>
2020-05-01 15:31:07 +0530

    Added functionality to register a service on server and discpver that service from another service

--- courseCatalouge/src/main/java/com/springimplant/course/CourseCatalougeApplication.java
+++ courseCatalouge/src/main/java/com/springimplant/course/CourseCatalougeApplication.java
@@ -2,8 +2,10 @@ package com.springimplant.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CourseCatalougeApplication {
 
 	public static void main(String[] args) {
--- courseCatalouge/src/main/java/com/springimplant/course/controller/CatalogController.java
+++ courseCatalouge/src/main/java/com/springimplant/course/controller/CatalogController.java
@@ -2,20 +2,36 @@ package com.springimplant.course.controller;
 
 import java.math.BigInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.springimplant.course.core.Course;
 
 @RestController
 public class CatalogController {
+	
+	@Autowired
+	private EurekaClient eurekaClient;
 
 	@GetMapping("/")
 	public String getCatalogHome() {
 		String courseAppMsg="";
 		String courseUrl="http://localhost:8080/";
+		RestTemplate restTemplate=new RestTemplate();
+		courseAppMsg=restTemplate.getForObject(courseUrl,String.class);
+		return("Welcome to Course Catalog "+courseAppMsg);
+	}
+	
+	@GetMapping("/services/cataloghome")
+	public String getCatalogHomeService() {
+		String courseAppMsg="";
+		InstanceInfo instanceInfo=eurekaClient.getNextServerFromEureka("springimplant-course-api",false);
+		String courseUrl=instanceInfo.getHomePageUrl();
 		RestTemplate restTemplate=new RestTemplate();
 		courseAppMsg=restTemplate.getForObject(courseUrl,String.class);
 		return("Welcome to Course Catalog "+courseAppMsg);
@@ -30,10 +46,32 @@ public class CatalogController {
 		return("Our Courses are "+courses);
 	}
 	
+	@GetMapping("/services/catalog")
+	public String getCatalogService() {
+		String courses="";
+		InstanceInfo instanceInfo=eurekaClient.getNextServerFromEureka("springimplant-course-api",false);
+		String courseUrl=instanceInfo.getHomePageUrl();
+		courseUrl+="/courses";
+		RestTemplate restTemplate=new RestTemplate();
+		courses=restTemplate.getForObject(courseUrl,String.class);
+		return("Our Courses are "+courses);
+	}
+	
 	@GetMapping("/catalogcourse/{id}")
 	public String getCatalogCourse(@PathVariable("id") BigInteger id) {
 		Course course=new Course();
 		String courseUrl="http://localhost:8080/"+id;
+		RestTemplate restTemplate=new RestTemplate();
+		course=restTemplate.getForObject(courseUrl,Course.class);
+		return("Our Course is "+ course.getCoursename()+" by "+course.getAuthor());
+	}
+	
+	@GetMapping("/services/catalogcourse/{id}")
+	public String getCatalogCourseService(@PathVariable("id") BigInteger id) {
+		Course course=new Course();
+		InstanceInfo instanceInfo=eurekaClient.getNextServerFromEureka("springimplant-course-api",false);
+		String courseUrl=instanceInfo.getHomePageUrl();
+		courseUrl+="/"+id;
 		RestTemplate restTemplate=new RestTemplate();
 		course=restTemplate.getForObject(courseUrl,Course.class);
 		return("Our Course is "+ course.getCoursename()+" by "+course.getAuthor());
