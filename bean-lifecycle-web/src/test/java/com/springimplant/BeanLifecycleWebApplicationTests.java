f781d95b1cbf8547a191c5814c38872e4f156aff
Gaurav Matta <gaurav.matta@gmail.com>
2021-09-03 20:00:56 +0530

    bean life cycle

---
+++ bean-lifecycle-web/src/test/java/com/springimplant/BeanLifecycleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springimplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLifecycleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
