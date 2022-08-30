5d6f9567452655d0dd0e25812ad77acaea571b95
Gaurav <Gaurav@host.docker.internal>
2022-08-30 20:14:45 +0530

    added spring project

---
+++ activiti-project/src/main/java/com/example/activitiproject/ActivitiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiProjectApplication.class, args);
+	}
+
+}
---
+++ activiti-project/src/test/java/com/example/activitiproject/ActivitiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activitiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
