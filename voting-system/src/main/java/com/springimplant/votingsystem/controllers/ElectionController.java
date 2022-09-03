4ff9c39802877bc5e8f6479b76d5b67da430a331
Gaurav <Gaurav@host.docker.internal>
2022-09-03 19:10:55 +0530

    Updated Election Controller

--- voting-system/src/main/java/com/springimplant/votingsystem/controllers/ElectionController.java
+++ voting-system/src/main/java/com/springimplant/votingsystem/controllers/ElectionController.java
@@ -2,8 +2,10 @@ package com.springimplant.votingsystem.controllers;
 
 import org.jboss.logging.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/election")
 public class ElectionController {
 
 	public final Logger logger=Logger.getLogger(ElectionController.class);
