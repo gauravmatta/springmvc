1bdf494078dfd280f3fef58100a73c0f3b7b19bc
Gaurav <Gaurav@host.docker.internal>
2022-09-03 19:01:00 +0530

    added Spring Cloud Gateway Server

---
+++ SpringCloudGatwayServer/src/main/java/com/springimplant/cloud/gateway/SpringCloudGatwayServer/SpringCloudGatwayServerApplication.java
@@ -0,0 +1,13 @@
+package com.springimplant.cloud.gateway.SpringCloudGatwayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatwayServerApplication.class, args);
+	}
+
+}
