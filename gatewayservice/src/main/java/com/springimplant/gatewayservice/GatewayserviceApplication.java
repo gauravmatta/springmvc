16086c6d37650cfbb993197b9f6d8aa2c4c23ada
Gaurav <Gaurav@host.docker.internal>
2022-09-30 02:07:42 +0530

    updated gateway with authorization service

--- gatewayservice/src/main/java/com/springimplant/gatewayservice/GatewayserviceApplication.java
+++ gatewayservice/src/main/java/com/springimplant/gatewayservice/GatewayserviceApplication.java
@@ -21,10 +21,12 @@ public class GatewayserviceApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(AppConstants.STUDENT_SERVICE_KEY,r->r.path("/api/student-service/**")
+				.route(AppConstants.STUDENT_SERVICE_KEY,r->r.path("/api/student/**")
 				.filters(f->f.stripPrefix(2)).uri("http://localhost:8003"))
-				.route(AppConstants.COURSE_SERVICE_KEY,r->r.path("/api/course-service/**")
+				.route(AppConstants.COURSE_SERVICE_KEY,r->r.path("/api/course/**")
 				.filters(f->f.stripPrefix(2)).uri("http://localhost:8002"))
+				.route(AppConstants.COURSE_SERVICE_KEY,r->r.path("/api/users/**")
+						.filters(f->f.stripPrefix(2)).uri("http://localhost:8003"))
 				.route(AppConstants.AUTH_SERVICE_KEY,r->r.path("/api/semauth/**")
 						.filters(f->f.stripPrefix(2)).uri("http://localhost:8081"))
 				.build();
