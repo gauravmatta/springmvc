a92c076049d32401412b31e395deb6ae76066c5e
Gaurav <Gaurav@host.docker.internal>
2022-11-02 12:18:42 +0530

    Added endpoint to return userdetails

--- spring-security-jwt/src/main/java/com/springimplant/jwt/api/controller/UserController.java
+++ spring-security-jwt/src/main/java/com/springimplant/jwt/api/controller/UserController.java
@@ -8,8 +8,10 @@ import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springimplant.jwt.api.entity.User;
 import com.springimplant.jwt.api.repository.UserRepository;
+import com.springimplant.jwt.api.service.CustomUserDetailService;
 
 @RestController
 @RequestMapping("/users")
@@ -26,6 +29,10 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
+
 	
 	@GetMapping
 	@RolesAllowed({"Scorecard"})
@@ -39,6 +46,12 @@ public class UserController {
 	public User findBysso(@PathVariable("id") String id){
 		Optional<User> userObjOptional = userRepository.findByUserId(id);
 		return userObjOptional.orElse(null);
+	}
+	
+	@GetMapping("/getuserdetails/{id}")
+	public ResponseEntity<UserDetails> getUserDetails(@PathVariable("id") String sso) {
+		UserDetails userDetails = customUserDetailService.loadUserByUsername(sso);
+		return ResponseEntity.status(HttpStatus.OK).body(userDetails);
 	}
 	
 	@PostMapping
