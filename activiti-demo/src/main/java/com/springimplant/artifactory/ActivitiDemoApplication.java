f78f64d30790392a7b1211c780bcbc3f8b97e908
gaurav <gaurav@gaurav-HP-Notebook>
2023-08-07 09:41:54 +0530

    updated project

--- activiti-demo/src/main/java/com/springimplant/artifactory/ActivitiDemoApplication.java
+++ activiti-demo/src/main/java/com/springimplant/artifactory/ActivitiDemoApplication.java
@@ -1,15 +1,10 @@
 package com.springimplant.artifactory;
 
-import javax.annotation.PostConstruct;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
 import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
@@ -18,13 +13,19 @@ import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
 import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
 import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
 import com.amazonaws.services.securitytoken.model.Credentials;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
-public class ArtifactoryApplication {
-
+public class ActivitiDemoApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(ActivitiDemoApplication.class);
 	
 	@PostConstruct
 	public void initAws() {
@@ -43,9 +44,9 @@ public class ArtifactoryApplication {
 		AmazonIdentityManagement clientAmazonIdentityManagement = AmazonIdentityManagementClientBuilder.standard().withRegion(region).withCredentials(credentialsProvider).build();
 		clientAmazonIdentityManagement.listRoles().getRoles().forEach(r -> log.info(r.getArn()));
 	}
-	
+
 	public static void main(String[] args) {
-		SpringApplication.run(ArtifactoryApplication.class, args);
+		SpringApplication.run(ActivitiDemoApplication.class, args);
 	}
 
 }
