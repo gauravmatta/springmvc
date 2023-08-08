8934c84da8b0bdc6a885c9239d9861dbf67b9323
gaurav <gaurav@gaurav-HP-Notebook>
2023-08-08 10:33:24 +0530

    Removed assume role

--- activiti-demo/src/main/java/com/springimplant/artifactory/ActivitiDemoApplication.java
+++ activiti-demo/src/main/java/com/springimplant/artifactory/ActivitiDemoApplication.java
@@ -24,26 +24,6 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 @Slf4j
 public class ActivitiDemoApplication {
-	
-	private static final Logger log = LoggerFactory.getLogger(ActivitiDemoApplication.class);
-	
-	@PostConstruct
-	public void initAws() {
-		String roleARNString = "arn:aws:iam::395527390279:role/av-sptm-console-role";
-		String roleSessionName = "Session_1";
-		Regions region = Regions.US_EAST_1;
-		AWSSecurityTokenService stsClient = AWSSecurityTokenServiceClientBuilder.standard().build();
-		AssumeRoleRequest roleRequest = new AssumeRoleRequest().withRoleArn(roleARNString).withRoleSessionName(roleSessionName).withDurationSeconds(3600);
-		AssumeRoleResult assumeRoleResult = stsClient.assumeRole(roleRequest);
-		Credentials credentials = assumeRoleResult.getCredentials();
-		log.info("Access Key "+credentials.getAccessKeyId());
-		log.info("Secret Access Key "+credentials.getSecretAccessKey());
-		log.info("Session Token "+credentials.getSessionToken());
-		AWSCredentials awscredentials = new BasicSessionCredentials(credentials.getAccessKeyId(), credentials.getSecretAccessKey(),credentials.getSessionToken());
-		AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(awscredentials);
-		AmazonIdentityManagement clientAmazonIdentityManagement = AmazonIdentityManagementClientBuilder.standard().withRegion(region).withCredentials(credentialsProvider).build();
-		clientAmazonIdentityManagement.listRoles().getRoles().forEach(r -> log.info(r.getArn()));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivitiDemoApplication.class, args);
