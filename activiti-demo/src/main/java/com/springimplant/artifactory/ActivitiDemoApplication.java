4b831fb108dad0a260c8551158d0b20b98bcf76e
gaurav <gaurav@gaurav-HP-Notebook>
2023-08-08 12:18:56 +0530

    Added activiti framework

--- activiti-demo/src/main/java/com/springimplant/artifactory/ActivitiDemoApplication.java
+++ activiti-demo/src/main/java/com/springimplant/artifactory/ActivitiDemoApplication.java
@@ -1,24 +1,8 @@
 package com.springimplant.artifactory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
-import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
-import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
-import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
-import com.amazonaws.services.securitytoken.model.Credentials;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
---
+++ activiti-demo/src/main/java/com/springimplant/artifactory/util/Beans.java
@@ -0,0 +1,22 @@
+package com.springimplant.artifactory.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class Beans {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+	    return new UserDetailsService() {
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+}
