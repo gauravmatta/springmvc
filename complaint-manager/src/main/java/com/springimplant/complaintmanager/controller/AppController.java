d761045c7c87678dbc5809d820e34444f5a359ca
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-09-13 12:50:33 +0530

    updated projects

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,7 +49,7 @@ public class AppController
     	return "fileComplaint";
     }
     
-    @RequestMapping("/submitComplaint")
+    @PostMapping("/submitComplaint")
     public String submitComplaint(
     		@RequestParam("complaint") String complaint,
     		@RequestParam("name") String name, 
