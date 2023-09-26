228ed7bb5083f393ad49b7f54942e3390f26522f
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-09-26 11:34:32 +0530

    updated collection properties

--- book management system/src/main/java/com/springimplant/model/Student.java
+++ book management system/src/main/java/com/springimplant/model/Student.java
@@ -1,5 +1,10 @@
 package com.springimplant.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +16,8 @@ public class Student {
 	private int studentId;
 	private String studentName;
 	private String studentAddress;
+	private List<String> weekAttendence;
+	private Set<String> subjects;
+	private Map<String,String> marks;
+	Properties attendenceKeys = new Properties();
 }
