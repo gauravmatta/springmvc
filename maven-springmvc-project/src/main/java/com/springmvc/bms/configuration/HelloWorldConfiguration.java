c5a1bc67c77a3161cf676f892c6ec3438bfb37a0
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-09-30 19:31:06 +0530

    Added annotation based configuration and beans

--- maven-springmvc-project/src/main/java/com/springmvc/bms/configuration/HelloWorldConfiguration.java
+++ maven-springmvc-project/src/main/java/com/springmvc/bms/configuration/HelloWorldConfiguration.java
@@ -1,5 +1,12 @@
 package com.springmvc.bms.configuration;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +16,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 @ComponentScan(basePackages="com.springmvc.bms")
 public class HelloWorldConfiguration {
 	@Bean
@@ -20,4 +27,35 @@ public class HelloWorldConfiguration {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+	
+	@Bean(name = "absentAttendence")
+	public List<String> presentAttendence() {
+		List<String> attendence = new LinkedList<String>();
+		attendence.add("0");
+		attendence.add("0");
+		attendence.add("0");
+		attendence.add("0");
+		attendence.add("0");
+		return attendence;
+	}
+	
+	@Bean(name = "alldistinction")
+	public Map<String,String> alldistinction() {
+		Map<String,String> marks = new HashMap<>();
+		marks.put("Maths", "75");
+		marks.put("English", "75");
+		marks.put("Hindi", "75");
+		marks.put("Knowledge", "75");
+		return marks;
+	}
+	
+	@Bean(name = "subjects")
+	public Set<String> subjects() {
+		Set<String> subjects = new LinkedHashSet<>();
+		subjects.add("Maths");
+		subjects.add("English");
+		subjects.add("Hindi");
+		subjects.add("Knowledge");
+		return subjects;
+	}
 }
--- maven-springmvc-project/src/main/java/com/springmvc/bms/main/Project.java
+++ maven-springmvc-project/src/main/java/com/springmvc/bms/main/Project.java
@@ -1,9 +1,18 @@
 package com.springmvc.bms.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.springmvc.bms.beans.Student;
+import com.springmvc.bms.configuration.HelloWorldConfiguration;
+
+
+
 public class Project {
-
 	public static void main(String[] args) {
-		
+		try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)) {
+			Student s1 = (Student) context.getBean("stu");
+			System.out.println(s1.toString());
+			context.close();
+		}
 	}
-
 }
