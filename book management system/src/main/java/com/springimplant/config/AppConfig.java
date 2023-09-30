601cf163f1350156eaba20324fde5c28d5e3025a
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-09-30 14:31:20 +0530

    Added Spring Bean Scopes

--- book management system/src/main/java/com/springimplant/config/AppConfig.java
+++ book management system/src/main/java/com/springimplant/config/AppConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.springimplant.model.Book;
+import com.springimplant.model.StereoType;
 import com.springimplant.model.Student;
 import com.springimplant.model.Subject;
 import com.springimplant.model.Teacher;
@@ -39,11 +40,14 @@ public class AppConfig {
 			Student s3 = (Student) context.getBean("Student3");
 			Subject sub1 = (Subject) context.getBean("Subject1");
 			logger.info(s1.toString());
+			System.out.println("Hashcode of Student s1 "+s1.hashCode());
 			logger.info("Class of S1 Week Attedence: "+s1.getWeekAttendence().getClass().getName());
 			logger.info(s2.toString());
+			System.out.println("Hashcode of Student s2 "+s2.hashCode());
 			logger.info("Class of S2 Week Attedence: "+s2.getWeekAttendence().getClass().getName());
 			logger.info(s2.getFavorites().getSubjectName());
 			logger.info(sp.toString());
+			System.out.println("Hashcode of Student sp "+sp.hashCode());
 			logger.info(s3.toString());
 			logger.info("Class of S3 Week Attedence: "+s3.getWeekAttendence().getClass().getName());
 			logger.info("Subjects===========>");
@@ -72,7 +76,18 @@ public class AppConfig {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/springimplant/xml/stereoconfig.xml")) {
 			context.registerShutdownHook();
 			Student s6 = context.getBean("stu",Student.class);
+			System.out.println("Hashcode of Student s6 "+s6.hashCode());
 			logger.info(s6.toString());
+			Student s7 = context.getBean("stu",Student.class);
+			System.out.println("Hashcode of Student s7 "+s7.hashCode());
+			StereoType st1 = context.getBean("stereo",StereoType.class);
+			System.out.println("Hashcode of Stereo Bean st1 "+st1.hashCode());
+			StereoType st2 = context.getBean("stereo",StereoType.class);
+			System.out.println("Hashcode of Stereo Bean st2 "+st2.hashCode());
+			StereoType st3 = context.getBean("stereo1",StereoType.class);
+			System.out.println("Hashcode of Stereo Bean st3 "+st3.hashCode());
+			StereoType st4 = context.getBean("stereo1",StereoType.class);
+			System.out.println("Hashcode of Stereo Bean st4 "+st4.hashCode());
 		}
 	}
 }
--- book management system/src/main/java/com/springimplant/model/StereoType.java
+++ book management system/src/main/java/com/springimplant/model/StereoType.java
@@ -1,5 +1,26 @@
 package com.springimplant.model;
 
-public class StereoType {
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("stereo")
+@Scope("prototype")
+public class StereoType {
+	@Value("Gaurav Matta")
+	private String studentName;
+	@Value("19c")
+	private String streetAddress;
+	@Value("#{absentAttendence}")
+	private List<String> weekAttendence;
+	@Value("#{subjects}")
+	private Set<String> subjects;
+	@Value("#{alldistinction}")
+	private Map<String,String> marks;
+	Properties attendenceKeys = new Properties();
 }
