b20f3c1b99c4dd1576f4e91afa731915155b64b8
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-06 23:44:28 +0530

    updated file list

--- my-project/src/main/java/com/springimplant/mvc/interceptors/GlobalInterceptor.java
+++ my-project/src/main/java/com/springimplant/mvc/interceptors/GlobalInterceptor.java
@@ -2,14 +2,15 @@ package com.springimplant.mvc.interceptors;
 
 import java.util.Date;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.springimplant.mvc.HitCounter;
+
+import jakarta.annotation.Resource;
+
 
 public class GlobalInterceptor extends HandlerInterceptorAdapter {
 	
@@ -21,10 +22,10 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 	
 	@Resource
 	private HitCounter sessionHit;
-	
+
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
 		RequestHit.setHits(RequestHit.getHits()+1);
 		System.out.println("Request Hits "+RequestHit.getHits());
 		applicationHit.setHits(applicationHit.getHits()+1);
@@ -32,8 +33,7 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 		sessionHit.setHits(sessionHit.getHits()+1);
 		System.out.println("Session Hits "+sessionHit.getHits());
 		// Moved to Controller Advice
-//		request.setAttribute("currentDate",new Date());
+		request.setAttribute("currentDate",new Date());
 		return super.preHandle(request, response, handler);
 	}
-
 }
