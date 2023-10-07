1775e860be9750f98aa34413ade6a70d66679c5e
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-07 15:51:19 +0530

    Enabled Transactional Management in Hibernate

--- RestfulWebServiceProject/src/main/java/com/springimplant/mvc/config/ProjectConfiguration.java
+++ RestfulWebServiceProject/src/main/java/com/springimplant/mvc/config/ProjectConfiguration.java
@@ -7,13 +7,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.springimplant.mvc.dao.StudentDao;
 import com.springimplant.mvc.model.Account;
 import com.springimplant.mvc.model.Student;
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages="com.springimplant.mvc")
 public class ProjectConfiguration {
 
@@ -22,6 +25,13 @@ public class ProjectConfiguration {
 		StudentDao dao = new StudentDao();
 		dao.setHibernateTemplate(getHibernateTemplate());
 		return dao;
+	}
+	
+	@Bean("transactionManager")
+	public HibernateTransactionManager getTmanger() {
+		HibernateTransactionManager mgr = new HibernateTransactionManager();
+		mgr.setSessionFactory(getFactoryBean().getObject());
+		return mgr;
 	}
 	
 	@Bean("ds")
@@ -42,7 +52,7 @@ public class ProjectConfiguration {
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
 		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl", "update");		
+		props.setProperty("hibernate.hbm2ddl.auto", "update");		
 		bean.setHibernateProperties(props);
 		Class<?> [] annotatedClasses =  {Student.class,Account.class};
 		bean.setAnnotatedClasses(annotatedClasses);
--- RestfulWebServiceProject/src/main/java/com/springimplant/mvc/dao/StudentDao.java
+++ RestfulWebServiceProject/src/main/java/com/springimplant/mvc/dao/StudentDao.java
@@ -1,16 +1,21 @@
 package com.springimplant.mvc.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.springimplant.mvc.model.Student;
 
 import lombok.Data;
 
 @Data
+@EnableTransactionManagement
 public class StudentDao {
 
 	private HibernateTemplate hibernateTemplate;
 	
+	@Transactional
 	public Integer insert(Student student) {
 		return (Integer)this.hibernateTemplate.save(student);
 	}
