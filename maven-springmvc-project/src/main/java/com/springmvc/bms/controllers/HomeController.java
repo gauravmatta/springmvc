ecaf3fbb8b5d57a9f2e196e66b4d01ae402c9708
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-10 12:01:25 +0530

    updated controller paths

--- maven-springmvc-project/src/main/java/com/springmvc/bms/controllers/HomeController.java
+++ maven-springmvc-project/src/main/java/com/springmvc/bms/controllers/HomeController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String gohome()
 	{
 		return "home";
