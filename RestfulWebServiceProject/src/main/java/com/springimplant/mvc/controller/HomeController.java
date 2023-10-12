3450812eec878b574193a431903798dc60e74a64
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-13 01:00:43 +0530

    added JSP Expression and JSTL Expression

--- RestfulWebServiceProject/src/main/java/com/springimplant/mvc/controller/HomeController.java
+++ RestfulWebServiceProject/src/main/java/com/springimplant/mvc/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.springimplant.mvc.controller;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,15 +14,28 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
 	@RequestMapping("/")
-	public String gohome()
+	public String gohome(Model model)
 	{
+		model.addAttribute("name","Gaurav Matta");
+		model.addAttribute("id",37);
+		List<String> friends = new ArrayList<>();
+		friends.add("Sumit");
+		friends.add("Arpit");
+		friends.add("Akshay");
+		model.addAttribute("friends", friends);
 		return "home";
 	}
 	
 	@RequestMapping("/index")
-	public String goindex()
+	public ModelAndView goindex()
 	{
-		return "index";
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("name","Samuel");
+		modelAndView.addObject("rollnumber",12);
+		LocalDateTime now = LocalDateTime.now();
+		modelAndView.addObject("time",now);
+		modelAndView.setViewName("index");
+		return modelAndView;
 	}
 	
 	@RequestMapping("/help")
--- RestfulWebServiceProject/src/main/webapp/WEB-INF/views/home.jsp
+++ RestfulWebServiceProject/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page import ="java.util.List"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +8,20 @@
 <title>Root Home</title>
 </head>
 <body>
-This is Root Home
+<% 
+String name = (String) request.getAttribute("name");
+Integer id = (Integer) request.getAttribute("id");
+List<String> friends = (List<String>)request.getAttribute("friends");
+%>
+<h1>This is Root Home of user <%=name%></h1>
+<h1>
+Id of user <%=id%>
+Friends are
+</h1>
+<%
+for(String f:friends) { 
+%>
+<h2><% out.println(f); %></h2>
+<% } %>
 </body>
 </html>
