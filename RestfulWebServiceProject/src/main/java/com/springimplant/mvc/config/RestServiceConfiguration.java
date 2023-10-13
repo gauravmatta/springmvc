bcfe0d0c3dc11e3d3d247e33c875fe69a11ec517
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-13 11:47:20 +0530

    updatd project

--- RestfulWebServiceProject/src/main/java/com/springimplant/mvc/config/RestServiceConfiguration.java
+++ RestfulWebServiceProject/src/main/java/com/springimplant/mvc/config/RestServiceConfiguration.java
@@ -3,7 +3,6 @@ package com.springimplant.mvc.config;
 import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -16,7 +15,7 @@ import com.springimplant.mvc.model.Student;
 
 @Configuration
 //@EnableWebMvc
-@ComponentScan(basePackages="com.springmvc.bms")
+//@ComponentScan(basePackages="com.springimplant.mvc")
 public class RestServiceConfiguration {
 	
 	@Bean(name="studentDao")
@@ -25,7 +24,6 @@ public class RestServiceConfiguration {
 		dao.setHibernateTemplate(getHibernateTemplate());
 		return dao;
 	}
-	
 	
 	@Bean("transactionManager")
 	public HibernateTransactionManager getTmanger() {
