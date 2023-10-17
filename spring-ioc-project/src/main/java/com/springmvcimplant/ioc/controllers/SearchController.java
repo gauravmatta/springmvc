f87cc2e7da888c76cd69d657b5004d48bf3a92ca
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-17 07:12:01 +0530

    updates search functionality

--- spring-ioc-project/src/main/java/com/springmvcimplant/ioc/controllers/SearchController.java
+++ spring-ioc-project/src/main/java/com/springmvcimplant/ioc/controllers/SearchController.java
@@ -2,10 +2,20 @@ package com.springmvcimplant.ioc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
+	
+	@RequestMapping("/lookup")
+	public RedirectView search(@RequestParam("querybox") String query) {
+		String url = "https://www.google.co.in?q="+query;
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(url);
+		return redirectView;
+	}
 
 	@RequestMapping("/home")
 	public String home() {
