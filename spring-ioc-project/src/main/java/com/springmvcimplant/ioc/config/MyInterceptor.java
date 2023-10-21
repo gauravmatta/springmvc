631ce83f37477b7ed32f1332be988b773010682a
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-10-21 23:23:33 +0530

    Updated Interceptor

--- spring-ioc-project/src/main/java/com/springmvcimplant/ioc/config/MyInterceptor.java
+++ spring-ioc-project/src/main/java/com/springmvcimplant/ioc/config/MyInterceptor.java
@@ -11,7 +11,7 @@ public class MyInterceptor  implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println("This is prehandler..");
+		System.out.println("This is Pre Handler..");
 		String name=request.getParameter("name");
 		if(name.startsWith("G")) {
 			response.setContentType("text/html");
@@ -24,14 +24,14 @@ public class MyInterceptor  implements HandlerInterceptor{
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("This is Post Handler...");
 		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("This is after completion method...");
 		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
 	}
 
