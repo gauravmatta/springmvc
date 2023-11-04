251f3448338c9b3513bca5efa6e5de53ade02458
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-11-04 20:03:48 +0530

    Added Internal Resource view resolver

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/config/MvcConfiguration.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/config/MvcConfiguration.java
@@ -3,7 +3,6 @@ package com.springimplant.complaintmanager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -11,17 +10,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 public class MvcConfiguration implements WebMvcConfigurer {
-	@Bean
-    public ViewResolver getViewResolver() {
+    @Bean
+    ViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setSuffix(".jsp");
 		resolver.setPrefix("/WEB-INF/jsp/");
         return resolver;
     }
-
-//    @Override
-//    public void configureDefaultServletHandling(
-//        DefaultServletHandlerConfigurer configurer) {
-//        configurer.enable();
-//    }    
 }
