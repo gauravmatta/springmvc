9469a6bcee77225dd40e3d5b21832de008a9736c
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-11-14 23:58:19 +0530

    updated spring boot app to support jsp

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
@@ -24,71 +24,9 @@ public class AppController
 	@Autowired
 	SessionFactory sessionFactory;
 	
-	@Autowired
-	private Environment env;
-	
-	@Autowired
-	Utils utils;
-	
     @GetMapping("/helloworld")
     public String helloWorld()
     {
     	return "HelloWorld";
-    }
-    
-    @GetMapping("/fileComplaint")
-    public String fileComplaint()
-    {
-    	Complaint complaint=new Complaint("Hello","Gaurav","gaurav@mail.com");
-    	Complaint complaint1=new Complaint("Test Message","Test","test@test.com");
-    	ComplaintDao complaintDao=new ComplaintDao(sessionFactory);
-    	complaintDao.insertComplaint(complaint);
-    	complaintDao.insertComplaint(complaint1);
-    	return "fileComplaint";
-    }
-    
-    @PostMapping("/submitComplaint")
-    public String submitComplaint(
-    		@RequestParam("complaint") String complaint,
-    		@RequestParam("name") String name, 
-    		@RequestParam("email") String email)
-    {
-    	//System.out.println(complaint);
-    	ComplaintDao dao=new ComplaintDao(sessionFactory);
-    	Complaint comp=new Complaint(complaint,name,email);
-    	dao.insertComplaint(comp);
-    	return "submitComplaint";
-    }
-    
-    @GetMapping(name="/showComplaints")
-    public String showComplaints()
-    {
-    	System.out.println("Here I am");
-    	return "showEnterPassword";
-    }
-    
-    @PostMapping(name="/showComplaints")
-    public ModelAndView showComplaintsPost(@RequestParam("pass") String pass,ModelAndView modelAndView)
-    {
-    	String syspass=env.getProperty("admin.password");
-    	String encodedPass=utils.md5Java(pass);
-    	if(encodedPass.equals(syspass))
-    	{
-         	ComplaintDao complaintDao=new ComplaintDao(sessionFactory);
-         	List<Complaint> complaints=complaintDao.getAllComplaints();
-         	modelAndView.addObject("complaints", complaints);
-         	modelAndView.setViewName("showComplaints");
-        	for(Complaint c:complaintDao.getAllComplaints())
-        	{
-        		System.out.println(c);
-        	}
-    	}
-    	else
-    	{
-    		modelAndView.setViewName("showEnterPassword");
-    	}
-    	
-    	return modelAndView;
-    	
     }
 }
