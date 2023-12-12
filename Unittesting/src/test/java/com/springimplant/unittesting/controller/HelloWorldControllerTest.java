8d03d773fa4a52f0d91edf6f8580a15ae3e76b88
Gaurav Matta <gaurav.matta.tech@gmail.com>
2023-12-12 10:49:00 +0530

    Added matchers in unit test

--- Unittesting/src/test/java/com/springimplant/unittesting/controller/HelloWorldControllerTest.java
+++ Unittesting/src/test/java/com/springimplant/unittesting/controller/HelloWorldControllerTest.java
@@ -1,6 +1,8 @@
 package com.springimplant.unittesting.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +13,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @ExtendWith(SpringExtension.class)
@@ -25,7 +29,32 @@ class HelloWorldControllerTest {
 		
 		RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);		
 		try {
-			MvcResult result = mockMvc.perform(request).andReturn();
+			MvcResult result = 
+					mockMvc.perform(request)
+					.andExpect(status().isOk())
+					.andExpect(content().string("Hello World"))
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andReturn();	
+			
+			
+//			   // Execute the GET request
+//	        mockMvc.perform(get("/rest/widget/{id}", 1L))
+//	                // Validate the response code and content type
+//	                .andExpect(status().isOk())
+//	                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//
+//	                // Validate headers
+//	                .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widget/1"))
+//	                .andExpect(header().string(HttpHeaders.ETAG, "\"1\""))
+//
+//	                // Validate the returned fields
+//	                .andExpect(jsonPath("$.id", is(1)))
+//	                .andExpect(jsonPath("$.name", is("Widget Name")))
+//	                .andExpect(jsonPath("$.description", is("Description")))
+//	                .andExpect(jsonPath("$.version", is(1)));
+			
+			
+			
 			assertEquals("Hello World",result.getResponse().getContentAsString());
 		} catch (Exception e) {
 			e.printStackTrace();
