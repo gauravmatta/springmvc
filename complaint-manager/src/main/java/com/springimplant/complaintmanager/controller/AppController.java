ed8e4e6b3b41cf2e59872f8a0ef783b34a366b40
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-04-09 09:48:50 +0530

    added docker compose and updated spring boot version

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
@@ -1,11 +1,16 @@
 package com.springimplant.complaintmanager.controller;
 
+import java.security.Principal;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class AppController
 {
 	@Autowired
@@ -14,6 +19,14 @@ public class AppController
     @GetMapping("/helloworld")
     public String helloWorld()
     {
+    	log.info("Hola ");
     	return "HelloWorld";
+    }
+    
+    @GetMapping("/")
+    public String home(Principal principal)
+    {
+    	log.info("Hola ");
+    	return "Hello, "+ principal.getName();
     }
 }
