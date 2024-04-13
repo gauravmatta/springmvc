5fc3e7f0a66c14274cd37735fa79460da224840d
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-04-13 22:39:59 +0530

    Enabled Hooks for Reactor Context

--- springredis/src/main/java/com/springimplant/springredis/SpringredisApplication.java
+++ springredis/src/main/java/com/springimplant/springredis/SpringredisApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.micrometer.context.ContextRegistry;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class SpringredisApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		ContextRegistry.getInstance().registerThreadLocalAccessor("cid",
 				() -> MDC.get("cid"),
 				cid->MDC.put("cid",cid),
