b0771cd458206d1c6d4f5233676a04fb26067883
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-04-15 10:35:11 +0530

    Updated Log Configuration

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/AppController.java
@@ -1,15 +1,22 @@
 package com.springimplant.complaintmanager.controller;
 
 import java.security.Principal;
+import java.util.Map;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.springimplant.complaintmanager.service.MathService;
 
 import jakarta.persistence.Entity;
 import lombok.extern.slf4j.Slf4j;
@@ -24,11 +31,15 @@ public class AppController
 	@Autowired
 	private WebClient webClient;
 	
+	@Autowired
+	@Qualifier("MathServiceImpl")
+	private MathService mathService;
+	
     @GetMapping("/helloworld")
-    public String helloWorld()
+    public Map<String,String> helloWorld(@RequestParam(defaultValue = "World") String name)
     {
-    	log.info("Hello World ");
-    	return "HelloWorld";
+    	log.info("Hello {}",name);
+    	return Map.of("greeting","Hello "+name);
     }
     
     @GetMapping("/")
@@ -47,4 +58,15 @@ public class AppController
     			.mapNotNull(HttpEntity::getBody)
     			.block();
     }
+    
+    @GetMapping("/addnumbers/{fnum}/{snum}")
+    public ResponseEntity<Integer> AddNumbers(
+    		@PathVariable("fnum") Integer firstNumber,
+    		@PathVariable("snum") Integer secondNumber
+    		){
+    	log.info("Adding Numbers");
+    	Integer sum = mathService.addNumbers(firstNumber, secondNumber);
+    	return new ResponseEntity<>(sum,HttpStatus.OK);
+    }
+    
 }
