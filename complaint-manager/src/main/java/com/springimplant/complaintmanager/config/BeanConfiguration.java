ad5819b82e767f0332d10828c57ec3d6490da770
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-05-31 20:09:48 +0530

    Added swagger configuration

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/config/BeanConfiguration.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/config/BeanConfiguration.java
@@ -8,11 +8,24 @@ import org.springframework.web.server.WebFilter;
 
 import jakarta.servlet.Filter;
 import reactor.util.context.Context;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class BeanConfiguration {
 	
 	String addressBaseUrl = "google.co.in";
+	
+//	 @Bean
+//	 Docket api() {
+//	        return new Docket(DocumentationType.SWAGGER_2)  
+//	          .select()
+//	          .apis(RequestHandlerSelectors.any())
+//	          .paths(PathSelectors.any())                 
+//	          .build();
+//	    }
 	
     @Bean
     WebClient webClient() {
