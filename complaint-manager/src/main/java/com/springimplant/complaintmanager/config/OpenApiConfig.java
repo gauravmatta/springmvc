6949cfca6fdc6d01486b7e86164bc73272652896
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-02 11:12:30 +0530

    updated configuration

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/config/OpenApiConfig.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
 	info = @Info (
@@ -19,7 +20,17 @@ import io.swagger.v3.oas.annotations.info.License;
 				name = "Your License No"
 				),
 		version = "v1"
-	)
+	),
+	servers = {
+			@Server(
+					description = "Dev",
+					url = "http://localhost:8081"
+					),
+			@Server(
+					description = "Test",
+					url = "http://localhost:8081"
+					)
+	}
 )
 public class OpenApiConfig {
 
