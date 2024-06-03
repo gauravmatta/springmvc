245a69ddd802f6f5a6231341f6365a9a4d7b850b
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-03 08:56:52 +0530

    added git oauth

---
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.springimplant.complaintmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+//	@Bean
+//	SecurityFilterChain securityFilterChain(HttpSecurity http) {
+//		try {
+//			return http
+//					.authorizeHttpRequests(auth -> {
+//						auth.requestMatchers("/").permitAll();
+//						auth.anyRequest().authenticated();
+//					})
+//					.oauth2Login(withDefaults())
+//					.formLogin(withDefaults())
+//					.build();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return null;
+//	}
+}
