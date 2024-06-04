17dd9a9ba9a8bf2d65ce2fe93b92d911f9beac82
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-04 10:54:32 +0530

    added bcrypt password encoding

--- course-api/src/main/java/com/springimplant/cms/config/AppSecurityConfig.java
+++ course-api/src/main/java/com/springimplant/cms/config/AppSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
@@ -21,7 +22,8 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     AuthenticationProvider authProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		return provider;
 	}
 
