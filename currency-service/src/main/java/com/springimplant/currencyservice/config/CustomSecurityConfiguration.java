0a48d9efc06cb2d04c6dbbf6ba81f5b0aafc4ace
gauravmatta <gaurav.matta.tech@gmail.com>
2024-06-23 18:45:22 +0530

    added error page

--- currency-service/src/main/java/com/springimplant/currencyservice/config/CustomSecurityConfiguration.java
+++ currency-service/src/main/java/com/springimplant/currencyservice/config/CustomSecurityConfiguration.java
@@ -1,13 +1,13 @@
 package com.springimplant.currencyservice.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
@@ -16,6 +16,7 @@ public class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
         http.authorizeRequests(a -> a
                         .antMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
@@ -29,7 +30,12 @@ public class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .logout(l -> l
         		.logoutSuccessUrl("/").permitAll()
         		)
-        .oauth2Login(withDefaults());
+        .oauth2Login(o -> o
+        		.failureHandler((request,response,exception) -> {
+        			request.getSession().setAttribute("error.message", exception.getMessage());
+        			handler.onAuthenticationFailure(request,response,exception);
+        		})
+        		);
 		
 	}
 }
--- currency-service/src/main/java/com/springimplant/currencyservice/controllers/CurrencyController.java
+++ currency-service/src/main/java/com/springimplant/currencyservice/controllers/CurrencyController.java
@@ -3,6 +3,8 @@ package com.springimplant.currencyservice.controllers;
 import java.util.Collections;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -24,5 +26,12 @@ public class CurrencyController {
 	@GetMapping("/user")
 	public Map<String, String> user(@AuthenticationPrincipal OAuth2User principal) {
 		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+	
+	@GetMapping("/error")
+	public String error(HttpServletRequest request) {
+		String message = (String)request.getSession().getAttribute("error.message");
+		request.getSession().removeAttribute("error.message");
+		return message; 
 	}
 }
