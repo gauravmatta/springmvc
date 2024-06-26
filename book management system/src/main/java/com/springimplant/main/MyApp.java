4490f4414ec552d3c61054f03dccc835dfcf6188
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-06-26 22:06:27 +0530

    Added code to create a file

--- book management system/src/main/java/com/springimplant/main/MyApp.java
+++ book management system/src/main/java/com/springimplant/main/MyApp.java
@@ -1,5 +1,8 @@
 package com.springimplant.main;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,14 +12,19 @@ import com.springimplant.config.HelloWorld;
 public class MyApp {
 
 	public static void main(String[] args) {
+		String parent =null;
+		File file = new File(parent,"myfile.txt");
 		try (ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("com/springimplant/xml/beans.xml")) {
 			context.start();
 			HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 			obj.getMessage();
+			file.createNewFile();
 		} catch (BeansException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
