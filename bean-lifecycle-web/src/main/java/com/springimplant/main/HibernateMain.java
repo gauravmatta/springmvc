bb8fa381e0722cecc412bd3d0a62698bd4ca596b
Gaurav <gaurav.matta.tech@gmail.com>
2024-07-02 01:04:11 +0530

    Added Main

---
+++ bean-lifecycle-web/src/main/java/com/springimplant/main/HibernateMain.java
@@ -0,0 +1,18 @@
+package com.springimplant.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.springimplant.util.HibernateUtil;
+
+public class HibernateMain {
+	
+	SessionFactory session = HibernateUtil.getSessionFactory();
+
+	public static void main(String[] args) {
+//		Transaction t = 
+
+	}
+
+}
