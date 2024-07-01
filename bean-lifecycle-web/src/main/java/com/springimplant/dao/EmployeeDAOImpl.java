de0ee81b6219c72a10263ab4df3921b15ecad841
Gaurav <gaurav.matta.tech@gmail.com>
2024-07-02 00:28:16 +0530

    Added Employee DAO

--- bean-lifecycle-web/src/main/java/com/springimplant/dao/EmployeeDAOImpl.java
+++ bean-lifecycle-web/src/main/java/com/springimplant/dao/EmployeeDAOImpl.java
@@ -2,20 +2,35 @@ package com.springimplant.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import com.springimplant.entity.Employee;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
+	
+	private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
 	@Override
 	public void save(Employee p) {
-		// TODO Auto-generated method stub
-
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(p);
+		tx.commit();
+		session.close();
 	}
 
 	@Override
 	public List<Employee> list() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = this.sessionFactory.openSession();
+		List<Employee> personList = session.createQuery("from Employee").list();
+		session.close();
+		return personList;
 	}
 
 }
