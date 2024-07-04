6d482349215eba23ea04b78183c20f0ef24a9267
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-04 16:47:15 +0530

    comment

--- bean-lifecycle-web/src/main/java/com/springimplant/BeanLifecycleWebApplication.java
+++ bean-lifecycle-web/src/main/java/com/springimplant/BeanLifecycleWebApplication.java
@@ -3,6 +3,7 @@ package com.springimplant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment
 @SpringBootApplication
 public class BeanLifecycleWebApplication {
 
