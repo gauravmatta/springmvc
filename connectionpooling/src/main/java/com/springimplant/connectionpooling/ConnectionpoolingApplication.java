d0cf061ee0c5bc9f85a6be8ed59e25dfb496e74b
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-05 13:45:59 +0530

    Added connection pooling

---
+++ connectionpooling/src/main/java/com/springimplant/connectionpooling/ConnectionpoolingApplication.java
@@ -0,0 +1,13 @@
+package com.springimplant.connectionpooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionpoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionpoolingApplication.class, args);
+	}
+
+}
