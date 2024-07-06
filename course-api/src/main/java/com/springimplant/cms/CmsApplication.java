e970adecdb3223cfbfe823414dfd69ac3a08d35a
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-06 20:20:53 +0530

    added treblle

--- course-api/src/main/java/com/springimplant/cms/CmsApplication.java
+++ course-api/src/main/java/com/springimplant/cms/CmsApplication.java
@@ -1,15 +1,23 @@
 package com.springimplant.cms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CmsApplication {
-
+public class CmsApplication implements CommandLineRunner {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();	
+		System.out.println("userb=>"+encoder.encode("admin"));
 	}
 
 }
