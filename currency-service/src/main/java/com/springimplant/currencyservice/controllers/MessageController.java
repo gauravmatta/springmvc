0d3c96db58b0f514b644dd514c9dbd0c02e50e95
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-10 21:35:07 +0530

    Added Delivery Partner App

--- currency-service/src/main/java/com/springimplant/currencyservice/controllers/MessageController.java
+++ currency-service/src/main/java/com/springimplant/currencyservice/controllers/MessageController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springimplant.currencyservice.contract.DeliveryPartnerData;
 import com.springimplant.currencyservice.contract.User;
+import com.springimplant.currencyservice.producer.DeliveryPartnerPhone;
 import com.springimplant.currencyservice.producer.KafkaJsonProducer;
 import com.springimplant.currencyservice.producer.KafkaProducer;
 
@@ -18,10 +20,12 @@ public class MessageController {
 	
 	private KafkaProducer kafkaProducer;
     private KafkaJsonProducer kafkaJsonProducer;
+    private DeliveryPartnerPhone deliveryPartnerPhone;
 
-    public MessageController(KafkaProducer kafkaProducer, KafkaJsonProducer kafkaJsonProducer) {
+    public MessageController(KafkaProducer kafkaProducer, KafkaJsonProducer kafkaJsonProducer,DeliveryPartnerPhone phone) {
         this.kafkaProducer = kafkaProducer;
         this.kafkaJsonProducer = kafkaJsonProducer;
+        this.deliveryPartnerPhone = phone;
     }
 
     @GetMapping("/publish")
@@ -34,5 +38,25 @@ public class MessageController {
     public ResponseEntity<String> publishJson(@RequestBody User user) {
         kafkaJsonProducer.publishMessage(user);
         return ResponseEntity.ok("JSON Message published successfully!");
+    }
+    
+    @GetMapping("/publishpartner")
+    public ResponseEntity<String> publishPartner() {
+    	DeliveryPartnerData dp1 = DeliveryPartnerData.builder()
+    			.orderId(1)
+    			.firstName("Lampard")
+    			.lastName("Messi")
+    			.distance(1500)
+    			.estimatedTime(5)
+    			.xCoordinte(Math.random()*(50-10)+10)
+    			.yCoordinte(Math.random()*(50-10)+10)
+    			.build();
+    	try {
+			deliveryPartnerPhone.publishDeliveryPartnerData(dp1);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+		}
+        return ResponseEntity.ok("Message published to the topic");
     }
 }
