25db388ecea2f377c4a685e1205a47d5089dde24
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-10 14:13:53 +0530

    Added Kafka Cluster Demo

--- currency-service/src/main/java/com/springimplant/currencyservice/config/CustomSecurityConfiguration.java
+++ currency-service/src/main/java/com/springimplant/currencyservice/config/CustomSecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.springimplant.currencyservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
