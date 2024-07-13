8b4725f6cde94fd9539651b2aef828448e43cd02
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-13 08:53:05 +0530

    Added Spring Cloud Config Server

--- spring-cloud-function-example/src/main/java/com/springimplant/cloudfunction/SpringCloudFunctionExampleApplication.java
+++ spring-cloud-function-example/src/main/java/com/springimplant/cloudfunction/SpringCloudFunctionExampleApplication.java
@@ -16,19 +16,19 @@ public class SpringCloudFunctionExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudFunctionExampleApplication.class, args);
 	}
-	
-	@Bean
-	public Function<String, String> reverse(){
+
+    @Bean
+    Function<String, String> reverse(){
 		return (input) -> new StringBuilder(input).reverse().toString();
 	}
-	
-	@Bean
-	public Supplier<Book> getBook(){
+
+    @Bean
+    Supplier<Book> getBook(){
 		return ()->new Book(101,"Core Java");
 	}
-	
-	@Bean
-	public Consumer<String> printMsg(){
+
+    @Bean
+    Consumer<String> printMsg(){
 		return (input)->System.out.println(input);
 	}
 
