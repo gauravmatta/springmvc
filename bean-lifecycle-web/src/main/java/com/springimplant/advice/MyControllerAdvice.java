4494dbabec634d2831149457a39fb74ba27c232d
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-07-17 15:22:56 +0530

    Added Response Status

--- bean-lifecycle-web/src/main/java/com/springimplant/advice/MyControllerAdvice.java
+++ bean-lifecycle-web/src/main/java/com/springimplant/advice/MyControllerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.springimplant.exceptions.EmptyInputException;
 
@@ -15,6 +16,7 @@ public class MyControllerAdvice {
 	}
 	
 	@ExceptionHandler(NullPointerException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST,reason = "Pass Number Only")
 	public ResponseEntity<String> handleNullInput(EmptyInputException emptyInputException){
 		return new ResponseEntity<>("Input field is Null",HttpStatus.BAD_REQUEST);
 	}
