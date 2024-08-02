f8abe27d61374bf8b90dbc98d23b8b12f5e15658
gauravmatta <gauravmatta@localhost>
2024-08-02 20:38:42 +0530

    Added Interceptors

---
+++ voting-system/src/main/java/com/springimplant/votingsystem/config/InterceptorConfig.java
@@ -0,0 +1,27 @@
+package com.springimplant.votingsystem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.springimplant.votingsystem.interceptor.GeneralInterceptor;
+import com.springimplant.votingsystem.interceptor.VotingInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	GeneralInterceptor generalInterceptor;
+	
+	@Autowired
+	VotingInterceptor votingInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(generalInterceptor).addPathPatterns("/");
+		registry.addInterceptor(votingInterceptor).addPathPatterns("/employees");
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+}
