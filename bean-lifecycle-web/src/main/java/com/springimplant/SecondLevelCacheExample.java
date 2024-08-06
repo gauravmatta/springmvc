acaa221780634c79e5c8273465ea0f7a474b1955
gauravmatta <gauravmatta@localhost>
2024-08-06 10:27:04 +0530

    Added Second Level Cache

--- bean-lifecycle-web/src/main/java/com/springimplant/SecondLevelCacheExample.java
+++ bean-lifecycle-web/src/main/java/com/springimplant/SecondLevelCacheExample.java
@@ -4,18 +4,26 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.springimplant.entity.Employee;
 import com.springimplant.entity.Student;
 
 public class SecondLevelCacheExample {
 	public static void main(String[] args) {
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		SessionFactory factory = new Configuration().configure()
+				.addAnnotatedClass(Student.class)
+				.addAnnotatedClass(Employee.class)
+				.buildSessionFactory();
 		Session session1 = factory.openSession();
-		Student student1 = session1.get(Student.class,12424);
+		Student student1 = session1.get(Student.class,1);
+		Employee emp1 = session1.get(Employee.class,103);
+		System.out.println(emp1);
 		System.out.println(student1);
 		session1.close();
 		
 		Session session2 = factory.openSession();
-		Student student2 = session1.get(Student.class,12424);
+		Student student2 = session2.get(Student.class,1);
+		Employee emp2 = session2.get(Employee.class,103);
+		System.out.println(emp2);
 		System.out.println(student2);
 		session2.close();
 		
--- bean-lifecycle-web/src/main/java/com/springimplant/entity/Student.java
+++ bean-lifecycle-web/src/main/java/com/springimplant/entity/Student.java
@@ -1,8 +1,13 @@
 package com.springimplant.entity;
 
 import javax.persistence.Cacheable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
@@ -11,11 +16,17 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "student", uniqueConstraints = {
+	    @UniqueConstraint(columnNames = "id")})
 @Cacheable
 @Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
 public class Student {
 	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	@Column(name="name")
 	private String name;
+	@Column(name="city")
 	private String city;
 }
