9d253792c9471e4f2f80af15abbc2fcefc20d1e7
gauravmatta <gauravmatta@localhost>
2024-08-07 19:51:28 +0530

    Added Hazelcast caching

--- voting-system/src/main/java/com/springimplant/votingsystem/VotingSystemApplication.java
+++ voting-system/src/main/java/com/springimplant/votingsystem/VotingSystemApplication.java
@@ -2,11 +2,12 @@ package com.springimplant.votingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableCaching
 //@ImportResource("classpath:app-config.xml")
 public class VotingSystemApplication {
 
--- voting-system/src/main/java/com/springimplant/votingsystem/controllers/VotingController.java
+++ voting-system/src/main/java/com/springimplant/votingsystem/controllers/VotingController.java
@@ -6,18 +6,25 @@ import javax.servlet.http.HttpSession;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.springimplant.votingsystem.entity.Candidate;
 import com.springimplant.votingsystem.entity.Citizen;
 import com.springimplant.votingsystem.repositories.CandidateRepo;
 import com.springimplant.votingsystem.repositories.CitizenRepo;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@CacheConfig(cacheNames= "candidates")
 public class VotingController {
 	
 	public final Logger logger=Logger.getLogger(VotingController.class);
@@ -35,6 +42,20 @@ public class VotingController {
 	{
 		logger.info("Voting Started");
 		return "vote.html";
+	}
+	
+	@GetMapping("/candidates")
+	@ResponseBody
+	public List<Candidate> getCandidates() {
+		return candidateRepo.findAll();
+	}
+	
+	@GetMapping("/candidate/{id}")
+	@ResponseBody
+	@Cacheable(key = "#id")
+	public Candidate getCandidate(@PathVariable Long id) {
+		logger.info("Fetching Candidate from DB with id "+id);
+		return candidateRepo.findById(id).orElse(null);
 	}
 	
 	@PostMapping("/casteVote")
