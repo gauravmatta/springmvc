c531e26e04f3739a079fc244e86cc135e81f38fa
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-08-10 08:43:27 +0530

    Added Spring Security

--- voting-system/src/main/java/com/springimplant/votingsystem/controllers/ElectionController.java
+++ voting-system/src/main/java/com/springimplant/votingsystem/controllers/ElectionController.java
@@ -2,11 +2,28 @@ package com.springimplant.votingsystem.controllers;
 
 import org.jboss.logging.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/election")
 public class ElectionController {
 
 	public final Logger logger=Logger.getLogger(ElectionController.class);
+	
+	@GetMapping("/")
+	public String home() {
+		return ("<h1>Welcome</h1>");
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1>Welcome User</h1>");
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1>Welcome Admin</h1>");
+	}
 }
