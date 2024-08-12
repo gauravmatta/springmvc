8d6e04dcae9bc2fafd890051e86dfb50987a80c3
gauravmatta <gaurav.matta.tech@gmail.com>
2024-08-12 10:01:25 +0530

    added HIbernate configuration

--- complaintsystem/src/main/java/com/springimplant/complaintsystem/controllers/AppController.java
+++ complaintsystem/src/main/java/com/springimplant/complaintsystem/controllers/AppController.java
@@ -1,14 +1,31 @@
 package com.springimplant.complaintsystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
 	
-	@RequestMapping("/helloworld")
-	public String helloWorld() {
-		return "HelloWorld";
+	@RequestMapping("/fileComplaint")
+	public String fileComplaint() {
+		return "fileComplaint";
+	}
+	
+	@RequestMapping("/submitComplaint")
+	public String submitComplaint() {
+		return "submitComplaint";
+	}
+	
+	@GetMapping(name="/showComplaints")
+	public String showComplaints() {
+		return "showEnterPassword";
+	}
+	
+	@PostMapping(name="/showComplaints")
+	public String showComplaintsPost() {
+		return "showComplaints";
 	}
 
 }
