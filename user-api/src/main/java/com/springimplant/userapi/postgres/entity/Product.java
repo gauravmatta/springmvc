afce9f23829a1f3c39b2c183c59bf1fa39b9d966
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-09-03 10:42:49 +0530

    Added Product Entity and Repository

---
+++ user-api/src/main/java/com/springimplant/userapi/postgres/entity/Product.java
@@ -0,0 +1,35 @@
+package com.springimplant.userapi.postgres.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String description;
+	
+	private boolean live;
+	
+	private double price;
+}
