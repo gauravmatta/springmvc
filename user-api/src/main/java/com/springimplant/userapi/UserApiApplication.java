1d34c9cf857906971318713226d455ec3ba75360
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-09-11 13:16:53 +0530

    updated service to register to Eureka Server

--- user-api/src/main/java/com/springimplant/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/springimplant/userapi/UserApiApplication.java
@@ -2,11 +2,7 @@ package com.springimplant.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,15 +11,4 @@ public class UserApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
-	
-@Configuration
-class config {
-	
-	@Bean
-//	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-}
-
 }
