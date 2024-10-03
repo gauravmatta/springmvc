20fd4c94053907f25fd623af4ce0128ef15248e5
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-10-03 07:36:30 +0530

    added test users in AppSecurityConfig

--- course-api/src/main/java/com/springimplant/cms/config/AppSecurityConfig.java
+++ course-api/src/main/java/com/springimplant/cms/config/AppSecurityConfig.java
@@ -1,19 +1,17 @@
 package com.springimplant.cms.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -24,14 +22,30 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
-    @Bean
+
+    @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication()
+    	.withUser("gaurav")
+    	.password("12345")
+    	.roles("USER")
+    	.and()
+    	.withUser("userTest")
+    	.password("test")
+    	.roles("ADMIN");
+	}
+
+	@Bean
     AuthenticationProvider authProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 //		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		return provider;
+	}
+	
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 	@Override
