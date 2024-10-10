def6381ab649166298c1439e1a073c6b2c46cef0
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-10-10 12:20:11 +0530

    Added Global Exception for Page not Found

--- user-api/src/main/java/com/springimplant/userapi/exceptions/GlobalExceptionHandler.java
+++ user-api/src/main/java/com/springimplant/userapi/exceptions/GlobalExceptionHandler.java
@@ -6,10 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.springimplant.userapi.contracts.ApiResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
@@ -26,6 +30,18 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(UsernameNotFoundException.class)
 	public ResponseEntity<ApiResponse> handlerUsernameNotFoundException(UsernameNotFoundException ex){
+		String message = ex.getMessage();
+		ApiResponse apiResponse = ApiResponse.builder()
+		.message(message)
+		.success(true)
+		.status(HttpStatus.NOT_FOUND)
+		.build();
+		return new ResponseEntity<>(apiResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<ApiResponse> handlerNoResourceFoundException(NoResourceFoundException ex){
+		log.info(ex.toString());
 		String message = ex.getMessage();
 		ApiResponse apiResponse = ApiResponse.builder()
 		.message(message)
