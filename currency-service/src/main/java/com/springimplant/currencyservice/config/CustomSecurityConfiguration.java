c6579653c293dddfaa57c90279a1af3fd31f00d8
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-10-14 11:40:39 +0530

    updated service

--- currency-service/src/main/java/com/springimplant/currencyservice/config/CustomSecurityConfiguration.java
+++ currency-service/src/main/java/com/springimplant/currencyservice/config/CustomSecurityConfiguration.java
@@ -2,18 +2,31 @@ package com.springimplant.currencyservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
 public class CustomSecurityConfiguration {
+	
+//	  @Autowired
+//	  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//	    auth
+//	      .ldapAuthentication()
+//	        .userDnPatterns("uid={0},ou=people")
+//	        .groupSearchBase("ou=groups")
+//	        .contextSource()
+//	          .url("ldap://localhost:8389/dc=springframework,dc=org")
+//	          .and()
+//	        .passwordCompare()
+//	          .passwordEncoder(new BCryptPasswordEncoder())
+//	          .passwordAttribute("userPassword");
+//	  }
+	
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
