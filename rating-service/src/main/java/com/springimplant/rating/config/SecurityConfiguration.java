40f00214a1c42958aaf357b262e901a2924cbfee
Gaurav Matta <gaurav.matta.tech@gmail.com>
2024-10-15 14:18:23 +0530

    added ldap auth

--- rating-service/src/main/java/com/springimplant/rating/config/SecurityConfiguration.java
+++ rating-service/src/main/java/com/springimplant/rating/config/SecurityConfiguration.java
@@ -1,27 +1,44 @@
 package com.springimplant.rating.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
 
 @EnableWebSecurity
 @Configuration
+@Slf4j
 public class SecurityConfiguration {
-	
-	  @Autowired
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorize) -> authorize.anyRequest().fullyAuthenticated())
+				.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
+
+	@Autowired
 	  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		log.info("#################");
+	    log.info("TEST");
+	    log.info("configuring ldap authenticaton");
+	    log.info("#################");
 	    auth
 	      .ldapAuthentication()
 	        .userDnPatterns("uid={0},ou=people")
 	        .groupSearchBase("ou=groups")
 	        .contextSource()
-	          .url("ldap://localhost:8389/dc=springframework,dc=org")
-	          .and()
+	        .url("ldap://localhost:8399/dc=springframework,dc=org")
+	        .and()
 	        .passwordCompare()
-	          .passwordEncoder(new BCryptPasswordEncoder())
-	          .passwordAttribute("userPassword");
+	        .passwordEncoder(new BCryptPasswordEncoder())
+	        .passwordAttribute("userPassword");
 	  }
-
 }
