0a4715f0ffd521ee52c5a2ef4f010cc555da020d
gauravmatta <gauravmatta@gaurav-matta-HP-Notebook>
2024-11-08 10:21:17 +0530

    updated project

--- user-api/src/main/java/com/springimplant/userapi/controllers/HomeController.java
+++ user-api/src/main/java/com/springimplant/userapi/controllers/HomeController.java
@@ -1,11 +1,9 @@
 package com.springimplant.userapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HomeController {
 
 	@GetMapping("/home")
